172dc51e7a85e46fb5133c61ce2426ce5bb91602
jayc51 <jayant.kulkarni@ibm.com>
2023-11-24 14:53:01 -0800

    adding DB2 calls

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask
+import sys
+import json
+import os, ibm_db_dbi as dbi, pandas as pd
+import requests
+import numpy as np
 import os
 
 app = Flask(__name__)
@@ -14,7 +19,7 @@ def other_hello():
     return "Hello my other World!"
 
 
-@app.route("/orders")
+@app.route("/orders/")
 def fetch_orders():
     db2_dsn = "DATABASE={};HOSTNAME={};PORT={};PROTOCOL=TCPIP;UID={uid};PWD={pwd};SECURITY=SSL".format(
         "bludb",
