f050c1c25c0d89cc7ce81c0f4118742e8e98de8c
jayc51 <jayant.kulkarni@ibm.com>
2023-11-24 14:43:58 -0800

    adding DB2 calls

--- app.py
+++ app.py
@@ -14,6 +14,24 @@ def other_hello():
     return "Hello my other World!"
 
 
+@app.route("/orders")
+def fetch_orders():
+    db2_dsn = "DATABASE={};HOSTNAME={};PORT={};PROTOCOL=TCPIP;UID={uid};PWD={pwd};SECURITY=SSL".format(
+        "bludb",
+        "fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud",
+        "32731",
+        uid="ktx43420",
+        pwd="Zbtqco8TYB0mzMui",
+    )
+
+    db2_connection = dbi.connect(db2_dsn)
+    query = "SELECT * FROM PROCUREMENT"
+    print(query)
+    df = pd.read_sql_query(query, con=db2_connection)
+    print(df)
+    return df
+
+
 if __name__ == "__main__":
     port = os.environ.get("FLASK_PORT") or 8080
     port = int(port)
